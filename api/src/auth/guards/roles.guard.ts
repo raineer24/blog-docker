ccdc6f30c72c49d4a857b55f557df9965b7ae000
raineer <delaritaraineer81@gmail.com>
2023-02-04 22:14:06 +0800

    role guard

--- api/src/auth/guards/roles.guard.ts
+++ api/src/auth/guards/roles.guard.ts
@@ -14,5 +14,19 @@ import { hasRoles } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor() {}
+  constructor(private reflector: Reflector, private userService: UserService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    console.log(request);
+    const user = request.user;
+    return true;
+  }
 }
