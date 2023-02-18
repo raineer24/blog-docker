47d017e67b4f5c7c2432fb6a0386501af9a6d3db
raineer <delaritaraineer81@gmail.com>
2023-02-18 13:49:22 +0800

    users components

--- frontend/src/app/public/components/user/users/users.component.ts
+++ frontend/src/app/public/components/user/users/users.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { map, tap } from 'rxjs/operators';
+import {
+  UserData,
+  UserService,
+} from '../../../../public/services/user-service/user.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  dataSource: UserData = null;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.initDataSource();
   }
 
+  initDataSource() {
+    this.userService.findAll(1, 10).pipe(
+      tap((users) => console.log(users)),
+      map((userData: UserData) => this.dataSource = userData)
+      ).subscribe();
+  }
 }
