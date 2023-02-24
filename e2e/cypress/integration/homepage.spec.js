5f0b5225efacf65b34485e2a4b4f4082932655ce
raineer <delaritaraineer81@gmail.com>
2023-02-24 11:16:04 +0800

    cypress user login

--- e2e/cypress/integration/homepage.spec.js
+++ e2e/cypress/integration/homepage.spec.js
@@ -4,4 +4,12 @@ describe('Homepage', () =>{
         cy.visit('/');
     });
 
+    it('should contain right spelled texts', () => {
+        cy.contains('Users');
+        cy.contains('Admin');
+        cy.contains('Login');
+        cy.get('mat-select').click();
+        cy.contains('Register');
+    });
+
 })
---
+++ e2e/cypress/integration/login.spec.js
@@ -0,0 +1,9 @@
+// const { USERNAME, PASSWORD } = require("./constants");
+
+// describe("", () => {
+//   cy.visit("/login");
+//   cy.get('[formcontrolname="email"]').type(USERNAME);
+//   cy.get('[formcontrolname="password"]').type(PASSWORD);
+//   cy.get('[type="submit"]').click();
+//   cy.url().should("include", "admin");
+// });
