3662794fe2c14f9bc3ee3d838b23141112b73095
raineer <delaritaraineer81@gmail.com>
2023-03-06 11:13:16 +0800

    user profile compoinent

--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -12,12 +12,14 @@ import {MatInputModule} from '@angular/material/input';
 import { UsersComponent } from './components/user/users/users.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
-    UsersComponent
+    UsersComponent,
+    UserProfileComponent
   ],
   imports: [
     CommonModule,
